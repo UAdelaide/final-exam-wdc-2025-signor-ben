a4b9bfbec3ef649cd45f00296029cbd04d5b2b11
ohnihili <bpfsignorelli@gmail.com>
2025-06-20 06:40:16 +0000

    [automatic commit] 2025-06-20 06:40:16 UTC 129.127.202.89

--- part1/app.js
+++ part1/app.js
@@ -29,11 +29,13 @@ async function insertTestData() {
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed789', 'owner')`);
+
         await db.execute(`INSERT IGNORE INTO Dogs (owner_id, name, size)
         SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123'`);
 
         await db.execute(`INSERT IGNORE INTO Dogs (owner_id, name, size)
         SELECT user_id, 'Bella', 'small' FROM Users WHERE username = 'carol123'`);
+
         await db.execute(`INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
         SELECT d.dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open'
         FROM Dogs d WHERE d.name = 'Max'`);
