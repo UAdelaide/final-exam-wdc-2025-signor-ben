55dfd34887234c3b52327968f73c0cc5214044b2
ohnihili <bpfsignorelli@gmail.com>
2025-06-20 12:37:01 +0000

    [automatic commit] 2025-06-20 12:37:01 UTC

---
+++ part2/routes/dogRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
+
+router.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM Dogs');
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
+module.exports = router;
