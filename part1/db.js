635175d42882a47837f5f9ad7b86660d28efcb77
ohnihili <bpfsignorelli@gmail.com>
2025-06-20 06:09:58 +0000

    [automatic commit] 2025-06-20 06:09:58 UTC 129.127.202.89

--- part1/db.js
+++ part1/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',           // or your MySQL host
-  user: 'root',                // change this to your MySQL username
-  password: '',                // change this to your MySQL password
-  database: 'DogWalkService',  // name from dogwalks.sql
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
