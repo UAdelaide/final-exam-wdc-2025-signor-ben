083bdde7d4a97f7acb873593c4fed0f23f793f1b
ohnihili <bpfsignorelli@gmail.com>
2025-06-20 12:36:41 +0000

    [automatic commit] 2025-06-20 12:36:41 UTC

--- part2/app.js
+++ part2/app.js
@@ -20,10 +20,13 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./routes/dogRoutes');
+
+
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
+app.use('/api/dogs', dogRoutes);
 
 
 // Export the app instead of listening here
