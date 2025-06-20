74e917c48aeb183179e3d5bf85b7fc36d75251f0
ohnihili <bpfsignorelli@gmail.com>
2025-06-20 07:35:40 +0000

    [automatic commit] 2025-06-20 07:35:40 UTC 129.127.202.89

--- part2/app.js
+++ part2/app.js
@@ -16,11 +16,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.use(session({
-  secret: 'superdogsecret',
-  resave: false,
-  saveUninitialized: true
-}));
+
 
 // Export the app instead of listening here
 module.exports = app;
