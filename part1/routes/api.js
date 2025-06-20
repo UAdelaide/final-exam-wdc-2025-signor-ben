33c0a7c930173a912642f1cb1a8e22be7affc875
ohnihili <bpfsignorelli@gmail.com>
2025-06-20 06:24:07 +0000

    [automatic commit] 2025-06-20 06:24:07 UTC 129.127.202.89

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -20,11 +20,11 @@ router.get('/dogs', async (req, res) => {
 router.get('/walkrequests/open', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location
+      SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, wr.owner_username
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       WHERE wr.status = 'open'
-      
+
     `);
     res.json(rows);
   } catch (err) {
